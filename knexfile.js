1dbf0746f3c3c70eec3e7bf2a854417df04aca68
nikitinskaya <nikitinskaya.v@gmail.com>
2021-01-29 21:56:34 +0300

    Init from template

---
+++ knexfile.js
@@ -0,0 +1,32 @@
+// @ts-check
+
+const path = require('path');
+
+const migrations = {
+  directory: path.join(__dirname, 'server', 'migrations'),
+};
+
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './database.sqlite',
+    },
+    useNullAsDefault: true,
+    migrations,
+  },
+  test: {
+    client: 'sqlite3',
+    connection: ':memory:',
+    useNullAsDefault: true,
+    migrations,
+  },
+  production: {
+    client: 'pg',
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    },
+    migrations,
+  },
+};
